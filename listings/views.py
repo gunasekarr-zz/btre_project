4b0f3a397e7c679385b16b7560b795d54ec5cc4a
guna <gunasekarr2000@yahoo.com>
2019-01-27 18:50:04 -0800

    django

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,5 @@
 ''' Python Classes for getting db code '''
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
 
@@ -18,7 +18,13 @@ def index(request):
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listings.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
