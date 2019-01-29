24a14132213493e6fdbe9c4048a23ad58c68cdd2
guna <gunasekarr2000@yahoo.com>
2019-01-28 22:24:24 -0800

    Removed email from settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -144,12 +144,6 @@ MESSAGE_TAGS = {
 
 }
 
-#Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'gunasekarr2000@gmail.com'
-EMAIL_PASSWORD = ''
-EMAIL_USE_TLS = True
 
 try:
     from .local_settings import *
